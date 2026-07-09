5336d1560ea69644428aa820cde9b2c7d03c09cd
rohit ranjan <rrohit2005@gmail.com>
2026-07-09 16:53:39 +0530

    Update Vite configuration to load environment variables for API base URL

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": "http://localhost:8000",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const BASE_URL = env.BASE_URL || env.VITE_BASE_URL || "http://localhost:5000";
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-  },
+  };
 });
